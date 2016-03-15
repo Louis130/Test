900cddababfc3c7c7a895898f2a7f2f67207ad93
Louis <loulou.bernon@gmail.com>
2016-03-15 20:06:20 +0100

    And the life begins with php*

---
+++ Navigation.php
@@ -0,0 +1,9 @@
+<nav>
+	<ul>
+		<li><a href=web.php?code=12>home</a></li>
+		<li><a href=web.php?code=12&page=3>big data</a></li>
+		<li><a href=web.php?code=12&page=2>heat control</a></li>
+		<li><a href=web.php?code=12&page=1>power supply</a></li>
+		<li><a href=web.php?code=12&page=4>about</a></li>
+	</ul>
+</nav>
